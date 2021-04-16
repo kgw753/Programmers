9341ad0e45992cd10b46a7bddc4df89ef57c5f72
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-04-16 21:58:23 +0900

    solved

---
+++ p64061.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class p64061 {
+    public static int solution(int[][] board, int[] moves) {
+        LinkedList<Integer> basket = new LinkedList<>();
+        int answer = 0;
+        for(int i = 0; i < moves.length; i++){
+            int now = moves[i] - 1;
+
+            System.out.println("*****"+now + " " + board.length);
+            
+            int j;
+            for(j = 0; j < board.length && board[j][now] == 0 ; j++){
+                
+            }
+            System.out.println("j = " + j);
+            if(j == board.length) continue;
+            
+
+            if(!basket.isEmpty() && basket.getLast() == board[j][now]){
+                basket.removeLast();
+                answer += 2;
+            }
+            else{
+                basket.add(board[j][now]);
+            }
+
+            board[j][now] = 0;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] moves = new int[8];
+        for(int i = 0; i < 8; i++){
+            moves[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(arr, moves));
+        
+    }
+}
