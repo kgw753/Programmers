47fdef06079e0ac9c057af1fb6743187dec0e7fd
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-04-27 17:03:09 +0900

    solved

--- p60057.java
+++ p60057.java
@@ -2,11 +2,9 @@ public class p60057 {
     public static int solution(String s) {
         int answer = s.length();
         
-        for(int i = 1; i < s.length() / 2; i++){
+        for(int i = 1; i <= s.length() / 2; i++){
             int len = compare(i, s).length();
             answer = Math.min(answer, len);
-            System.out.println();
-            System.out.println(compare(i, s));
         }
 
         return answer;
@@ -14,25 +12,33 @@ public class p60057 {
 
     public static String compare(int comp, String s){
         String str = "";
-        for(int i = 0; i < s.length() - comp; i++){
-            String s1 = s.substring(i, i + comp);
+        for(int i = 0; i < s.length() + comp; i += comp){
+            String s1 = "";
+
+            if(i + comp < s.length()){
+                s1 = s.substring(i, i + comp);
+            }
+            else if(i < s.length()){
+                s1 = s.substring(i, s.length());
+            }
+            else{
+                break;
+            }
+
             int cnt = 1;
             int idx = i + comp;
+
             while(idx + comp <= s.length() && s1.equals(s.substring(idx, idx + comp))){
                 cnt++;
                 idx += comp;
+                i += comp;
             }
 
             if(cnt > 1){
-                i += (cnt - 1);
                 str += (Integer.toString(cnt) + s1);
             }
             else{
-                str += s.charAt(i);
-                if(i + comp >= s.length()){
-                    str += s.substring(i + 1, s.length());
-                }
-
+                str += s1;
             }
         }
         return str;
@@ -40,7 +46,8 @@ public class p60057 {
 
     public static void main(String[] args){
         System.out.println(solution("aabbaccc"));
-        System.out.println(solution("abcabcabcabcdededededede"));
+        System.out.println(solution("abcabcdede"));
+        System.out.println(solution("xababcdcdababcdcd"));
         
     }
 }
