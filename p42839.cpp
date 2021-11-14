7ae5935d3a7dabbe9089ffbd2ce3897fe32f6433
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-14 14:51:20 +0900

    p42839

--- p42839.cpp
+++ p42839.cpp
@@ -1,23 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 10000004
-bool checkPrime(int n){
-    if(n == 0 || n == 1) return false;
-    for(int i = 2; i <= sqrt(n); i++){
-        if(n % i == 0) return false;
+bool check(int num){
+    if(num == 1 || num == 0) return false;
+    for(int i = 2; i <= sqrt(num); i++){
+        if(num % i == 0) return false;
     }
     return true;
 }
 int solution(string numbers) {
     set<int> st;
-    int num;
     sort(numbers.begin(), numbers.end());
     do{
         for(int i = 1; i <= numbers.length(); i++){
-            num = stoi(numbers.substr(0, i));
-            if(checkPrime(num)) st.insert(num);
+            int num = stoi(numbers.substr(0, i));
+            if(check(num)){
+                st.insert(num);
+            }
         }
     }while(next_permutation(numbers.begin(), numbers.end()));
+    for(int i : st) cout << i << "\n";
     return st.size();
 }
 int main(){
