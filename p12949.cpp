06c9c6687268a1402ae582043a93a80ea764ba30
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-14 15:58:11 +0900

    p12949

---
+++ p12949.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    int row = arr1.size();
+    int col = arr2[0].size();
+    vector<int> tmp(col, 0);
+    for(int i = 0; i < row; i++){
+        answer.push_back(tmp);
+    }
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            for(int k = 0; k < arr1[0].size(); k++){
+                answer[i][j] += (arr1[i][k] * arr2[k][j]);
+            }
+        }
+    }
+    return answer;
+}
