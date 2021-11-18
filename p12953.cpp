9868f2d2ed77789753fdbeef10258e03725591fa
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-18 17:15:57 +0900

    p12953

--- p12953.cpp
+++ p12953.cpp
@@ -1,25 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int GCD(int a, int b){
-    if(b == 0) return a;
-    return GCD(b, a % b);
+    if(a == 0) return b;
+    else return GCD(b % a, a);
 }
-int LCM(int a, int b){
+int LSM(int a, int b){
     return (a * b) / GCD(a, b);
 }
 int solution(vector<int> arr) {
     int ret = 1;
-    for(int num : arr){
-        ret = LCM(ret, num);
+    for(int n : arr){
+        ret = LSM(ret, n);
     }
     return ret;
-}
-int main(){
-    int N;
-    cin >> N;
-    vector<int> arr(N);
-    for(int i = 0; i < N; i++){
-        cin >> arr[i];
-    }
-    cout << solution(arr) << "\n";
 }
