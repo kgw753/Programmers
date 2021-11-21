defb1caa78eb013ff92436e1285a77854043b1a6
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-21 13:47:35 +0900

    p86052

--- p86052.cpp
+++ p86052.cpp
@@ -1,36 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 504
-int MAP[MAX][MAX];
 int N, M;
-bool visited[MAX][MAX][4];
-int dx[] = {1, 0, -1, 0};
 int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+int MAP[MAX][MAX];
+bool visited[MAX][MAX][4];
+int getNPos(int pos, int mx){
+    if(pos == mx) return 0;
+    else if(pos < 0) return mx - 1;
+    else return pos;
+}
 int go(int y, int x, int dir, int len){
     if(visited[y][x][dir]) return len;
     visited[y][x][dir] = true;
-
     int nd = (dir + MAP[y][x] + 4) % 4;
-    int ny = y + dy[nd];
-    int nx = x + dx[nd];
-
-    if(ny < 0) ny = N - 1;
-    if(nx < 0) nx = M - 1;
-    if(ny == N) ny = 0;
-    if(nx == M) nx = 0;
-
+    int ny = getNPos(y + dy[nd], N);
+    int nx = getNPos(x + dx[nd], M);
     return go(ny, nx, nd, len + 1);
 }
-vector<int> solution(vector<string> grid) {
-    vector<int> ret;
-    N = grid.size();
-    M = grid[0].length();
+void setMap(vector<string> grid){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
             if(grid[i][j] == 'L') MAP[i][j] = 1;
             else if(grid[i][j] == 'R') MAP[i][j] = -1;
+            else MAP[i][j] = 0;
         }
     }
+}
+vector<int> solution(vector<string> grid){
+    vector<int> ret;
+    N = grid.size();
+    M = grid[0].size();
+    setMap(grid);
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
             for(int k = 0; k < 4; k++){
@@ -41,15 +43,4 @@ vector<int> solution(vector<string> grid) {
     }
     sort(ret.begin(), ret.end());
     return ret;
-}
-int main(){
-    int N;
-    cin >> N;
-    vector<string> v(N);
-    for(int i = 0; i < N; i++){
-        cin >> v[i];
-    }
-    vector<int> res = solution(v);
-    for(int r : res) cout << r << " ";
-    cout << "\n";
 }
