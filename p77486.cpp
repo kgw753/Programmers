e159c2853585bcfd26b4e3bdff7283d524c38528
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-11-25 17:28:28 +0900

    p77486

--- p77486.cpp
+++ p77486.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-map<string, int> profit;
 map<string, string> parent;
+map<string, int> profit;
 void update(string name, int money){
     if(name == "-") return;
     int nMoney = money * 0.1;
-    profit[name] += (money - nMoney);
     if(money < 1) return;
+    profit[name] += (money - nMoney);
     update(parent[name], nMoney);
+    return;
 }
 vector<int> solution(vector<string> enroll, vector<string> referral, 
                         vector<string> seller, vector<int> amount) {
-    vector<int> answer;
+    vector<int> ret;
     for(int i = 0; i < enroll.size(); i++){
         parent[enroll[i]] = referral[i];
     }
@@ -19,7 +20,7 @@ vector<int> solution(vector<string> enroll, vector<string> referral,
         update(seller[i], amount[i] * 100);
     }
     for(string name : enroll){
-        answer.push_back(profit[name]);
+        ret.push_back(profit[name]);
     }
-    return answer;
+    return ret;
 }
